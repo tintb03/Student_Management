55fb7a62976fd0c662729661f28263de4af225b4
tintb03 <128471718+tintb03@users.noreply.github.com>
2023-08-08 18:25:12 +0700

    add seeder admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Tạo tài khoản admin
+        // php artisan db:seed --class=AdminSeeder
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('123123123'),
+            'role' => 'admin',
+        ]);
+    }
+}
